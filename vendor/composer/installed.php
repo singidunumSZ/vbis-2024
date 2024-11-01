238ed99971f166e5a5269f00b16522c5521e7172
sara zivkovic <sarazivkovic1004@icloud.com>
2024-11-01 19:34:56 +0100

    postavljen projekat

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'petar/vbis2024',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b320d0960e7563f165130105eca0e04e2154336c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'petar/vbis2024' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b320d0960e7563f165130105eca0e04e2154336c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
