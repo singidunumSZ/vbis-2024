6112fbd38cc4c45967e60a750139ffca21dcbd76
sara zivkovic <sarazivkovic1004@icloud.com>
2024-11-02 19:07:56 +0100

    napravljen header u view

--- views/home.php
+++ views/home.php
@@ -5,6 +5,14 @@
 
     <title>Home page</title>
 </head>
+<body>
+<header style = "width : 100%; background-color: pink;color: antiquewhite">
+    header
+</header>
 <h1>WELCOME PAGE</h1>
+
+</body>
+
+
 
 </html>
