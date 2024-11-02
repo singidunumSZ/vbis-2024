ea0a267882e8dfc326af0e4e48229ae28a93e132
sara zivkovic <sarazivkovic1004@icloud.com>
2024-11-02 13:47:05 +0100

    napravljen view i metoda render

---
+++ core/View.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\core;
+
+class View
+{
+    public function render($viewName){
+        ob_start();
+        include_once  __DIR__."/..views/$viewName.php";
+        return ob_get_clean();
+    }
+}
