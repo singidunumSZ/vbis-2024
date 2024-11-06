c6558cdba870e3c6e742b577efdeeb0efb91d59e
sara zivkovic <sarazivkovic1004@icloud.com>
2024-11-07 00:31:34 +0100

    konekcija sa bp

---
+++ core/DbConnection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\core;
+
+class DbConnection
+{
+    public function connect(){
+        $mysqli = new mysqli("localhost", "root", "", "vbis");
+        $dbResult = $mysqli->query("select * from users");
+        $user = $dbResult->fetch_assoc();
+        return $mysqli;
+    }
+}
