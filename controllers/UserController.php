de78b16b57ecf07b6d1843f463ab7ebbddb05365
sara zivkovic <sarazivkovic1004@icloud.com>
2024-11-07 01:24:49 +0100

    konekcija sa bp uspesno

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,17 +4,24 @@ namespace app\controllers;
 use app\core\BaseController;
 use app\core\View;
 use app\models\UserModel;
-
+use app\core\DbConnection;
 class UserController extends BaseController
 {
 
 
     public function readUser()
     {
+        $db = new DbConnection();
+        $con = $db->connect();
+        $dbResult = $con->query("select * from users limit 1");
+        $user = $dbResult->fetch_assoc();
+
+
+
         $model = new UserModel();
-        $model->email ='sara.zivkovic.22@singimail.rs';
-        $model-> firstName ='Sara';
-        $model->lastName ='Zivkovic';
+        $model->email =$user['email'];
+        $model-> firstName = $user['first_name'];
+        $model->lastName = $user['last_name'];
 
 
 
--- core/DbConnection.php
+++ core/DbConnection.php
@@ -5,7 +5,7 @@ namespace app\core;
 class DbConnection
 {
     public function connect(){
-        $mysqli = new mysqli("localhost", "root", "", "vbis");
+        $mysqli = new \mysqli("localhost", "root", "", "vbis");
         $dbResult = $mysqli->query("select * from users");
         $user = $dbResult->fetch_assoc();
         return $mysqli;
